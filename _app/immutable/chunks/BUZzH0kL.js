47056644059b7bad7b99df79da54df62635424c6
baek-si-hyun <qortlgus100@gmail.com>
2025-10-23 11:21:32 +0900

    Updates

---
+++ _app/immutable/chunks/BUZzH0kL.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_18i5n9w?.base??"/invest-web",e=globalThis.__sveltekit_18i5n9w?.assets??s??"";export{e as a,s as b};
--- _app/immutable/chunks/cldChT0N.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_12hwkrg?.base??"/invest-web",e=globalThis.__sveltekit_12hwkrg?.assets??s??"";export{e as a,s as b};
